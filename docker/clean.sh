d634163d86889ab4c5e73359e93941ec974fad34
Luis Scoccola <luis.scoccola@gmail.com>
2025-11-27 14:07:45 -0500

    chore: untrack unnecessary files

--- docker/clean.sh
+++
@@ -1,2 +0,0 @@
-
-rm -r /Users/luisscoccola/code/agentic-lean/docker/root-directory/*
--- git_lean_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git --git-dir=/Users/luisscoccola/code/agentic-lean/autoformalization.git \
-    --work-tree=/Users/luisscoccola/code/agentic-lean/workspace/lean_project "$@"
